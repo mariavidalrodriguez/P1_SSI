2fae68db0ae632b19caab0d5c18405dae06263a1
mariavidalrodriguez <maria.vidal.rodriguez@udc.es>
2022-09-25 16:26:52 +0200

    Initial commit

---
+++ cesar.py
@@ -0,0 +1,33 @@
+# Introducimos el texto a cifrar
+texto=input("Introduzca el texto a cifrar: ")
+
+#Creamos la cadena de caracteres
+if texto==texto.upper():
+    alf="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+else:
+    alf="abcdefghijklmnopqrstuvwxtz"
+
+#Definimos el valor del desplazamiento
+k=int(input("Valor de desplazamiento: "))
+
+#Creamos la cadena donde almacenará el texto cifrado
+cifrad=""
+
+#Cifrado
+for c in texto:
+    if c in alf:
+        cifrad += alf[(alf.index(c)+k)%(len(alf))]
+    else:
+        cifrad +=c
+
+print(cifrad)
+
+# Descifrado
+descif = ""
+for c in cifrad:
+    if c in alf:
+        descif += alf[(alf.index(c) - k) % (len(alf))]
+    else:
+        descif += c
+
+print(descif)
---
+++ main.py
@@ -0,0 +1,34 @@
+#Algoritmo de cifrado simétrico por sustitución monoalfabeto
+
+# Introducimos el texto a cifrar
+texto=input("Introduzca el texto a cifrar: ")
+
+#Creamos la cadena de caracteres
+if texto==texto.upper():
+    alfb_cif ="UVWXYZNOPQRSTHIJKLMABCDEFG"
+    alfabeto ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+else:
+    alfb_cif="uvwxyznopqrsthijklmabcdefg"
+    alfabeto = "abcdefghijklmnopqrstuvwxtz"
+
+#Creamos la cadena donde almacenará el texto cifrado
+cifrad=""
+descif=""
+
+#Cifrado
+for c in texto:
+    if c in alfabeto:
+        cifrad += alfb_cif[alfabeto.index(c)]
+    else:
+        cifrad +=c
+
+print(cifrad)
+
+#Descifrado
+for c in cifrad:
+    if c in alfb_cif:
+        descif += alfabeto[alfb_cif.index(c)]
+    else:
+        descif += c
+
+print(descif)
