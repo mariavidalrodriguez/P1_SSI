0b3514ef95a96ffc37fb9898c21c2e43f254ef12
mariavidalrodriguez <maria.vidal.rodriguez@udc.es>
2022-09-25 17:42:09 +0200

    Algoritmo de cifrado y descifrado simétrico por sustitución monoalfabeto

--- monoalfabeto.py
+++ monoalfabeto.py
@@ -1,37 +1,42 @@
-#Algoritmo de cifrado simétrico por sustitución monoalfabeto
+# Algoritmo de cifrado simétrico por sustitución monoalfabeto
 
 import sys
 
-print(sys.argv)
+texto = open(sys.argv[1])  # seleccionamos el archivo .txt
+contenido = texto.read()
 
-texto = sys.argv[3]
-
-#Creamos la cadena de caracteres
-if texto==texto.upper():
-    alfb_cif ="UVWXYZNOPQRSTHIJKLMABCDEFG"
-    alfabeto ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# Creamos la cadena de caracteres
+if contenido == contenido.upper():
+    alfb_cif = "UVWXYZNOPQRSTHIJKLMABCDEFG"
+    alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 else:
-    alfb_cif="uvwxyznopqrsthijklmabcdefg"
+    alfb_cif = "uvwxyznopqrsthijklmabcdefg"
     alfabeto = "abcdefghijklmnopqrstuvwxtz"
 
-#Creamos la cadena donde almacenará el texto cifrado
+# Creamos la cadena donde almacenará el texto cifrado
 cifrad=""
 descif=""
 
-#Cifrado
-for c in texto:
-    if c in alfabeto:
-        cifrad += alfb_cif[alfabeto.index(c)]
-    else:
-        cifrad +=c
+# Seleccionamos la acción que queremos realizar, cifrar o descifrar un texto
+accion = input("Elija una opción:1) Cifrar 2) Descifrar\n")
 
-print(cifrad)
+if accion == "1":
+    #Cifrado
 
-#Descifrado
-for c in cifrad:
-    if c in alfb_cif:
-        descif += alfabeto[alfb_cif.index(c)]
-    else:
-        descif += c
+    for c in contenido:
+        if c in alfabeto:
+            cifrad += alfb_cif[alfabeto.index(c)]
+        else:
+            cifrad += c
+    print(cifrad)
+elif accion == "2":
+    # Descifrado
+    for c in contenido:
+        if c in alfb_cif:
+            descif += alfabeto[alfb_cif.index(c)]
+        else:
+            descif += c
+    print(descif)
+else:
+    print("No ha pulsado un número válido")
 
-print(descif)
