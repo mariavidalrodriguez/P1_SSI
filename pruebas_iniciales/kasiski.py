a02fe2f16650d860a502160a78205ce927eb94bd
mariavidalrodriguez <maria.vidal.rodriguez@udc.es>
2022-10-04 12:09:14 +0200

    Cambios generales

--- pruebas_iniciales/kasiski.py
+++ pruebas_iniciales/kasiski.py
@@ -1,6 +1,7 @@
 # Implementación del método Kasiski
 
 import sys
+import re
 
 alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
@@ -13,5 +14,34 @@ for n in contenido: # Comprobamos que solo hay caracteres válidos en el texto i
         break
 
 # Buscamos en el texto cadenas de 3 caracteres que se repitan y guardamos las distancias entre 2 cadenas repetidas
+
+cadenas_rep = []
+cadena = contenido[0:3]
+
+text_sep = [] # aqui guardamos cada cadena de longitud 3
+for i in range(0, len(contenido), 3):
+    text_sep.append(contenido[i:i+3])
+# print("Texto separado:", text_sep)
+
+# Obtenemos las cadenas de 3 letras que se repiten en nuestro texto
+i = 0
+desplz = []
+for pos in text_sep:
+    first = contenido.find(contenido[i:i+3]) # devuelve la posición del primer elemento encontrado
+    last = contenido.rfind(contenido[i:i+3]) # devuelve la posición del último elemento encontrado
+    if first == last:
+        first = []
+        last = []
+    else:
+        if contenido[i:i+3] not in cadenas_rep:
+            cadenas_rep.append(contenido[i:i+3])
+            result = last - first
+            desplz.append(result)
+            print("Cadena:", contenido[i:i+3], "repetida: ", contenido.count(contenido[i:i+3]), "veces")
+    i = i + 3
+
+print("Cadenas repetidas:", cadenas_rep)
+print("Desplazamiento: ", desplz)
+
 
 
